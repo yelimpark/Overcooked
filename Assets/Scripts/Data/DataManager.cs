cbc8e5c486fd1f2e086f825ff40d383cd9ea82cc
Byeongrok <webqud30@gmail.com>
2022-07-20 19:08:26 +0900

    Fixed Scriptable Object

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -9,17 +9,14 @@ using Newtonsoft.Json.Serialization;
 
 public class DataManager : MonoBehaviour
 {
-    public List<SceneInfo> currentQuestDataList;
+    public List<SceneInfo> currentStageInfo;
 
     private FileInfo fileInfo;
 
-    private int currentSlot;
-
-    public void LoadStageData(int slotNum)
+    public void LoadStageData()
     {
-        string fileName = "StageInfo" + slotNum;
-        string path = Application.dataPath + "/" + "AATeamProject" + "/" +
-            "Json" + "/" + fileName + ".Json";
+        string fileName = "StageInfo";
+        string path = Application.dataPath + "/" + "Json" + "/" + fileName + ".Json";
 
         fileInfo = new FileInfo(path);
 
@@ -28,39 +25,36 @@ public class DataManager : MonoBehaviour
             Debug.Log($"[DataManager] 세이브 데이터 없음");
 
             fileName = "DefaultQuestData";
-            path = Application.dataPath + "/" + "AATeamProject" + "/" +
-            "Json" + "/" + fileName + ".Json";
+            path = Application.dataPath + "/" + "Json" + "/" + fileName + ".Json";
 
             string json = File.ReadAllText(path);
 
-            currentQuestDataList = JsonConvert.DeserializeObject<List<SceneInfo>>(json);
+            currentStageInfo = JsonConvert.DeserializeObject<List<SceneInfo>>(json);
         }
         else
         {
             string json = File.ReadAllText(path);
-            currentQuestDataList = JsonConvert.DeserializeObject<List<SceneInfo>>(json);
+            currentStageInfo = JsonConvert.DeserializeObject<List<SceneInfo>>(json);
         }
 
         Debug.Log($"[DataManager] 불러온 파일명 :  {fileName}");
-        currentSlot = slotNum;
+
     }
 
     public void SaveQuestData()
     {
-        string fileName = "Quest" + currentSlot;
-        string path = Application.dataPath + "/" + "AATeamProject" + "/" +
-            "Json" + "/" + fileName + ".Json";
+        string fileName = "StageInfo";
+        string path = Application.dataPath + "/" + "Json" + "/" + fileName + ".Json";
 
-        var setJson = JsonConvert.SerializeObject(currentQuestDataList);
+        var setJson = JsonConvert.SerializeObject(currentStageInfo);
         File.WriteAllText(path, setJson);
         Debug.Log($"[DataManager] 저장한 파일명 : {fileName}");
     }
 
-    public void DeleteQuestData(int slotNum)
+    public void DeleteQuestData()
     {
-        string fileName = "Quest" + slotNum;
-        string path = Application.dataPath + "/" + "AATeamProject" + "/" +
-           "Json" + "/" + fileName + ".Json";
+        string fileName = "StageInfo";
+        string path = Application.dataPath + "Json" + "/" + fileName + ".Json";
 
         File.Delete(path);
     }
