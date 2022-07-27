ff14d26aa9dd0dae2c5568bff5c0d567534bd86f
choiyunhwa <sjo4208@naver.com>
2022-07-27 19:03:14 +0900

    Update: Map2 Scene
    Map2 Movement

--- Assets/Scripts/InGameScene/Map/MapMove.cs
+++ Assets/Scripts/InGameScene/Map/MapMove.cs
@@ -7,7 +7,13 @@ public class MapMove : MonoBehaviour
     public Transform targetPosition;
     public Transform originPosition;
 
+    private Transform origin;
     private Vector3 vel = Vector3.zero;
+
+    private void Awake()
+    {
+        origin = this.gameObject.transform;
+    }
     private void Update()
     {
         StartCoroutine(CoMapMove());
@@ -18,11 +24,10 @@ public class MapMove : MonoBehaviour
 
         yield return new WaitForSeconds(5f);
         transform.position = Vector3.SmoothDamp(gameObject.transform.position, targetPosition.transform.position, ref vel, 1f);
-        Debug.Log(transform.position);
+        //transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition.transform.position, 1f);
 
         yield return new WaitForSeconds(5f);
-        transform.position = Vector3.SmoothDamp(gameObject.transform.position, originPosition.transform.position, ref vel, 1f);
-        Debug.Log(transform.position);
-
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, origin.position, ref vel, 1f);
+        //transform.position = Vector3.MoveTowards(gameObject.transform.position, origin.position, 1f);
     }
 }
