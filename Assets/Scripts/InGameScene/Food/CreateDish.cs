17e33860e80b6b028e1b908efc0e20c0a57204ee
Byeongrok <webqud30@gmail.com>
2022-08-05 18:57:15 +0900

    [2022.08.05] Create Dish

--- Assets/Scripts/InGameScene/Food/CreateDish.cs
+++ Assets/Scripts/InGameScene/Food/CreateDish.cs
@@ -2,26 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateDish : MonoBehaviour
+public class CreateDish : Interactable, ITakeOut
 {
-    //음식이 계산대에 들어갔을때 시간이 지나면 해당 위치에 생성 할 수 있도록
-
     public float timer;
+    public Transform spawn;
+    public ObjectPoolManager poolManager;
+    public bool startTimer = false;
 
-    private CreateIngredient createObject;
+    public List<GameObject> dishes = new List<GameObject>();
 
-
-    private void Start()
+    public void Update()
     {
-        createObject = GetComponent<CreateIngredient>();
+        if (startTimer) 
+            timer += Time.deltaTime;
+
+        if (timer > 4f)
+        {
+            timer = 0f;
+            var po = poolManager.Extract("Dish");
+            var go = po.gameObject;
+            dishes.Add(go);
+
+            Transform transform = go.transform;
+            Vector3 dishPos = spawn.position;
+            dishPos.y += (transform.lossyScale.y * dishes.Count - transform.lossyScale.y * 0.5f);
+            transform.position = dishPos;
+
+            Rigidbody rb = go.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.isKinematic = true;
+
+            var colliders = go.GetComponents<Collider>();
+            foreach (Collider collider in colliders)
+            {
+                collider.enabled = false;
+            }
+
+            go.transform.rotation = Quaternion.identity;
+
+            startTimer = false;
+        }
     }
 
     public void GenerateDish()
     {
-        timer += Time.deltaTime;
-        if (timer > 4f)
+        startTimer = true;
+    }
+
+    public bool TakeOut(EquipmentSystem es)
+    {
+        GameObject takeout = dishes[dishes.Count - 1];
+        dishes.Remove(takeout);
+
+        Rigidbody rb = takeout.GetComponent<Rigidbody>();
+        if (rb != null)
+            rb.isKinematic = false;
+
+        var colliders = takeout.GetComponents<Collider>();
+        foreach (Collider collider in colliders)
         {
-            createObject.Create();
+            collider.enabled = true;
         }
+
+        es.Equip(takeout);
+
+        return true;
     }
 }
--- Assets/Scripts/InGameScene/KitchenManager.cs
+++ Assets/Scripts/InGameScene/KitchenManager.cs
@@ -94,7 +94,7 @@ public class KitchenManager : MonoBehaviour
     public void OnSubmit(GameObject go)
     {
         cm.OnSubmit(go);
-        //createDish.GenerateDish(); //접시 타이밍
+        createDish.GenerateDish(); //접시 타이밍
     }
 
     public void GetScore(int score, bool isFever)
